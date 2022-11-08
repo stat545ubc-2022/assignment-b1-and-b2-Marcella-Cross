85c7236897caf89a3f8aeab77bc00f0ccfa6bfcb
Marcella_Cross <mjc467@student.ubc.ca>
2022-11-08 14:27:52 -0800

    Still Troubleshooting the data set

--- group.filterNA.summary/tests/testthat/test-group.R
+++ group.filterNA.summary/tests/testthat/test-group.R
@@ -1,11 +1,11 @@
 test_that("Testing group-filterNA-summary function", {
-  expect_equal(digest(group.filterNA.summary(van_tree_data, "curb")), digest(van_tree_data %>% group_by(curb) %>% drop_na("curb") %>%summarise(Count = n())))
-  expect_equal(digest(group.filterNA.summary(van_tree_data, "neighbourhood_name")), digest(van_tree_data %>% group_by(neighbourhood_name) %>% drop_na("neighbourhood_name") %>%summarise(Count = n())))
-  expect_error(group.filterNA.summary(van_tree_data, 10))
+  expect_equal(digest(group.filterNA.summary(treedata, "curb")), digest(treedata %>% group_by(curb) %>% drop_na("curb") %>%summarise(Count = n())))
+  expect_equal(digest(group.filterNA.summary(treedata, "neighbourhood_name")), digest(treedata %>% group_by(neighbourhood_name) %>% drop_na("neighbourhood_name") %>%summarise(Count = n())))
+  expect_error(group.filterNA.summary(treedata, 10))
 })
 
 test_that("Testing group-filterNA-summary function", {
-  expect_equal(digest(group.filterNA.summary(van_tree_data, "height_range_id")), digest(van_tree_data %>% group_by(height_range_id) %>% drop_na("height_range_id") %>%summarise(Count = n())))
-  expect_equal(digest(group.filterNA.summary(van_tree_data, "cultivar_name")), digest(van_tree_data %>% group_by(cultivar_name) %>% drop_na("cultivar_name") %>%summarise(Count = n())))
-  expect_equal(digest(group.filterNA.summary(van_tree_data, "diameter")), digest(van_tree_data %>% group_by(diameter) %>% drop_na("diameter") %>%summarise(Count = n())))
+  expect_equal(digest(group.filterNA.summary(treedata, "height_range_id")), digest(treedata %>% group_by(height_range_id) %>% drop_na("height_range_id") %>%summarise(Count = n())))
+  expect_equal(digest(group.filterNA.summary(treedata, "cultivar_name")), digest(treedata %>% group_by(cultivar_name) %>% drop_na("cultivar_name") %>%summarise(Count = n())))
+  expect_equal(digest(group.filterNA.summary(treedata, "diameter")), digest(treedata %>% group_by(diameter) %>% drop_na("diameter") %>%summarise(Count = n())))
 })
