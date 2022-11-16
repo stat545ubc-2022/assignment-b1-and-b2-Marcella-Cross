a7dd240199f0dcb78b5cd2160cd96474474b34ae
Marcella_Cross <mjc467@student.ubc.ca>
2022-11-16 09:00:24 -0800

    troubleshooting check() errors with group_by_() vs. group_by()

--- group.filterNA.summary/R/group.R
+++ group.filterNA.summary/R/group.R
@@ -2,14 +2,15 @@
 #'
 #' @param data -> this is the name of the data set
 #' @param column -> this is the name of the column/variable of interest
-#'
-#' @return summary table of counts for each value
+#' @description This package takes a column and data frame input and removes all NA values from that column. Next, it creates a summary table with the count of each value in the column. This function is used when you are interested in a count summary table of values within one column of your data.
+#' @return Summary table of counts for each value
+#' @author Marcella Cross - marcella.cross@botany.ubc.ca - ORCID id: 0000-0002-3866-6970
 #' @export
 #'
 #' @examples
-#'group.filterNA.summary(treedata, "street_side_name") #expect summary table of numbers of counts of EVEN vs. ODD
-#'group.filterNA.summary(treedata, "neighbourhood_name") #expect summary table of numbers of counts of neighbourhoods
-#'group.filterNA.summary(treedata, "curb") #expect summary table of numbers of counts of neighbourhoods
+#'group.filterNA.summary(treedata, "street_side_name") #expect summary table with EVEN vs. ODD
+#'group.filterNA.summary(treedata, "neighbourhood_name") #expect summary of neighbourhoods
+#'group.filterNA.summary(treedata, "curb") #expect summary of curb yes vs. no
 
 group.filterNA.summary <- function(data,column) {if(is.numeric("column")) {
   stop('Sorry, but this function works only for non-numeric inputs!\n',
---
+++ group.filterNA.summary/R/treedata.R
@@ -0,0 +1,6 @@
+#' Data of Trees in the City of Vancouver, Canada
+#'
+#' Variables include information regarding the trees in the city as well as information on their location and proximity to things such as curbs or root barriers. The data set also includes information on tree diameter and height class along with different names of the tree.
+#'
+#' @format A tibble with 146,611 rows and 20 variables:
+treedata
